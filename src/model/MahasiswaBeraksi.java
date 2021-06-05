82feccf9415eb78acd6923f427672e70ed085092
stwahyuu <septiantw27@gmail.com>
2021-06-05 19:37:27 +0700

    Membuat Class,Atribut, dan Objek

--- src/model/MahasiswaBeraksi.java
+++ src/model/MahasiswaBeraksi.java
@@ -8,7 +8,7 @@ import java.util.Date;
 public class MahasiswaBeraksi {
     public static void main(String[] args) throws ParseException {
 
-        String polaTanggal = "dd-MM_yyyy";
+        String polaTanggal = "dd-MM-yyyy";
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(polaTanggal);
 
         Mahasiswa mahasiswa =new Mahasiswa();
@@ -18,12 +18,12 @@ public class MahasiswaBeraksi {
 
         System.out.println("Nama \t\t\t : " + mahasiswa.nama);
         System.out.println("Npm \t\t\t : " + mahasiswa.npm);
-        System.out.println("Tanggal Lahir \t : " + mahasiswa.tanggalLahir);
+        System.out.println("Tanggal Lahir \t : " + simpleDateFormat.format(mahasiswa.tanggalLahir));
 
         Mahasiswa mahasiswa2 =new Mahasiswa();
-        mahasiswa2.npm = "19630125";
+        mahasiswa2.npm = "19630126";
         mahasiswa2.nama = "Aman Rohman";
-        mahasiswa2.tanggalLahir = simpleDateFormat.parse("26-09-2001");
+        mahasiswa2.tanggalLahir = simpleDateFormat.parse("09-09-2001");
 
         System.out.println("Nama \t\t\t : " + mahasiswa2.nama);
         System.out.println("Npm \t\t\t : " + mahasiswa2.npm);
